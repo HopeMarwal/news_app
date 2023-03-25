7edee162cf18240e4435567cdcd1b4de94c38bbe
Nadejda <vremea_denighi@mail.ru>
2023-03-25 19:15:46 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const ipToken = 'a532296877f3d8'
+  const accuWeatherToken = 'slIlACVHV0hMvoQA15SWVvGjN2B2yCEy'
+
+  const [forecastData, setForecastData] = useState([])
+
+  useEffect(() => {
+    const handleIpRequest = async () => {
+      const ipRequest = await fetch(`https://ipinfo.io/json?token=${ipToken}`)
+      const jsonIpResponse = await ipRequest.json()
+      const queryCoord = jsonIpResponse.loc
+
+      const locationKeyRequest = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${accuWeatherToken}&q=${queryCoord}`)
+      const jsonKeyResponse = await locationKeyRequest.json()
+      const key = jsonKeyResponse.Key
+
+      const forecastRequest = await fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${key}?apikey=${accuWeatherToken}`)
+      const jsonForecastRes = await forecastRequest.json()
+      setForecastData(jsonForecastRes.DailyForecasts[0])
+
+    }
+    handleIpRequest()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
     </div>
   );
 }
